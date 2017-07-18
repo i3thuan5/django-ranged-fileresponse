d5e4f6f419ce25736c101f93450fbbfe093e8d39
薛丞宏 <ihcaoe@gmail.com>
2017-07-18 11:51:10 +0800

    改package名

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 description = 'Modified Django FileResponse that adds Content-Range headers.'
 
 setup(
-    name='django-ranged-fileresponse',
-    version='0.1.1',
+    name='django-ranged-file-response',
+    version='0.2.0',
     description=description,
     url='https://github.com/wearespindle/django-ranged-fileresponse',
     author='Spindle',
