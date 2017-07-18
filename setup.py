0aaeb732e440b27beac04c74c3bfca44c4e01073
薛丞宏 <ihcaoe@gmail.com>
2017-07-18 16:26:18 +0800

    改號做ranged-response，因為無一定需要FileResponse

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 description = 'Modified Django FileResponse that adds Content-Range headers.'
 
 setup(
-    name='django-ranged-file-response',
+    name='django-ranged-response',
     version='0.2.0',
     description=description,
     url='https://github.com/wearespindle/django-ranged-fileresponse',
     author='Spindle',
     author_email='jeroen@wearespindle.com',
     license='MIT',
-    packages=['ranged_fileresponse'],
+    packages=['ranged_response'],
     install_requires=[
         'django',
     ],
